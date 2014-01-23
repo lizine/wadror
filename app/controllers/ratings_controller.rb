2dc6c9466bb7e3e5575e076864c5395a6b098150
Nina Liljeblad <nina.liljeblad@gmail.com>
2014-01-23 16:26:15 +0200

    voi lisata uuden reittauksen

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,4 +2,14 @@ class RatingsController < ApplicationController
 def index
   @ratings = Rating.all
 end
+
+def new
+    @rating = Rating.new
+    @beers = Beer.all
+  end
+  
+  def create
+      Rating.create params.require(:rating).permit(:score, :beer_id)
+      redirect_to "http://www.cs.helsinki.fi"
+  end
 end
