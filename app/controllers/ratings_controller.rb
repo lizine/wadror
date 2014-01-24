a116ef0a91b55d9cac72218b0fc15e2be92509e5
Nina Liljeblad <nina.liljeblad@gmail.com>
2014-01-24 15:14:02 +0200

    keskiarvometodit ennen moduuliksi muuttamista

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -12,4 +12,10 @@ def new
       Rating.create params.require(:rating).permit(:score, :beer_id)
       redirect_to "http://www.cs.helsinki.fi"
   end
+
+	def destroy
+	rating = Rating.find(params[:id])
+	rating.delete
+	redirect_to ratings_path
+	end
 end
