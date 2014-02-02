97a50c28b7e61d53ec7ca42399677ef7e233d130
Lauri-Pekka <ninapekka@ubuntu.(none)>
2014-02-02 22:09:06 +0200

    Salasanassa vaaditaan iso kirjain, numero, sekä vähintään 4 merkkiä

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ActiveRecord::Base
 
  validates :username, uniqueness: true
   validates :username, length: { in: 3..15 }
+#validates :password, length: { minimum: 4 }
+validates :password, :format => {:with => /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,}/, message: "must be at least 4 characters and include one number and one upper case letter."}
 
 
 def to_s
