fe8d3ca613e094cf9dffbee92ccc7e9652260a98
Lauri-Pekka <ninapekka@ubuntu.(none)>
2014-02-16 22:32:44 +0200

    lisätty ravintolatiedoille enimmäiseliniäksi minuutti

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,7 +1,8 @@
 class BeermappingApi
   def self.places_in(city)
     city = city.downcase
-    Rails.cache.fetch(city) { fetch_places_in(city) }
+    Rails.cache.fetch(city, :expires_in => 10.seconds) { fetch_places_in(city) }
+    
   end
 
   private
